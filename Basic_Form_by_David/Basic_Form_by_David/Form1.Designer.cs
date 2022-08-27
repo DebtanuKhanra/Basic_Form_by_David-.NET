a64dca69c9c1128de2e474ba192477a6ec7ffe97
DebtanuKhanra <debtanukhanra2000@gmail.com>
2022-08-27 14:17:59 +0530

    First Commit

---
+++ Basic_Form_by_David/Basic_Form_by_David/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Basic_Form_by_David
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textJobid = new System.Windows.Forms.TextBox();
+            this.textFname = new System.Windows.Forms.TextBox();
+            this.textLname = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textSal = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnInsert.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(313, 353);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(99, 41);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnUpdate.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(433, 353);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(97, 41);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(551, 353);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 41);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Job ID.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Email";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(35, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Salary";
+            // 
+            // textJobid
+            // 
+            this.textJobid.BackColor = System.Drawing.SystemColors.Control;
+            this.textJobid.Font = new System.Drawing.Font("Lucida Fax", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textJobid.Location = new System.Drawing.Point(169, 72);
+            this.textJobid.Name = "textJobid";
+            this.textJobid.Size = new System.Drawing.Size(223, 26);
+            this.textJobid.TabIndex = 8;
+            this.textJobid.TextChanged += new System.EventHandler(this.textJobid_TextChanged);
+            // 
+            // textFname
+            // 
+            this.textFname.BackColor = System.Drawing.SystemColors.Control;
+            this.textFname.Font = new System.Drawing.Font("Lucida Fax", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFname.Location = new System.Drawing.Point(169, 123);
+            this.textFname.Name = "textFname";
+            this.textFname.Size = new System.Drawing.Size(223, 26);
+            this.textFname.TabIndex = 9;
+            // 
+            // textLname
+            // 
+            this.textLname.BackColor = System.Drawing.SystemColors.Control;
+            this.textLname.Font = new System.Drawing.Font("Lucida Fax", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLname.Location = new System.Drawing.Point(169, 170);
+            this.textLname.Name = "textLname";
+            this.textLname.Size = new System.Drawing.Size(223, 26);
+            this.textLname.TabIndex = 10;
+            // 
+            // textEmail
+            // 
+            this.textEmail.BackColor = System.Drawing.SystemColors.Control;
+            this.textEmail.Font = new System.Drawing.Font("Lucida Fax", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEmail.Location = new System.Drawing.Point(169, 220);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(223, 26);
+            this.textEmail.TabIndex = 11;
+            // 
+            // textSal
+            // 
+            this.textSal.BackColor = System.Drawing.SystemColors.Control;
+            this.textSal.Font = new System.Drawing.Font("Lucida Fax", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSal.Location = new System.Drawing.Point(169, 266);
+            this.textSal.Name = "textSal";
+            this.textSal.Size = new System.Drawing.Size(223, 26);
+            this.textSal.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Font = new System.Drawing.Font("Lucida Fax", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(277, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(243, 32);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "!!   WELCOME   !!";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.label7.Font = new System.Drawing.Font("Lucida Fax", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(32, 358);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(261, 32);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "David\'s Database";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Yellow;
+            this.btnSearch.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(658, 353);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(88, 41);
+            this.btnSearch.TabIndex = 15;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(413, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(333, 223);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textSal);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textLname);
+            this.Controls.Add(this.textFname);
+            this.Controls.Add(this.textJobid);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textJobid;
+        private System.Windows.Forms.TextBox textFname;
+        private System.Windows.Forms.TextBox textLname;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textSal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Basic_Form_by_David/Basic_Form_by_David/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Basic_Form_by_David
+{
+    public partial class Form1 : Form
+    {
+        public void mustinput() // By this operation we cann't skip any Input box
+        {
+            if (textJobid.Text == "")
+            {
+                MessageBox.Show("Please Type  JobId");
+                textJobid.Focus();
+            }
+            else if (textFname.Text == "")
+            {
+                MessageBox.Show("Please Type  First Name");
+                textFname.Focus();
+            }
+            else if (textLname.Text == "")
+            {
+                MessageBox.Show("Please Type  Last Name");
+                textLname.Focus();
+            }
+            else if (textEmail.Text == "")
+            {
+                MessageBox.Show("Please Type  Email");
+                textEmail.Focus();
+            }
+            else if (textSal.Text == "")
+            {
+                MessageBox.Show("Please Type  Salary");
+                textSal.Focus();
+            }
+        }
+
+
+
+        public void scommand(SqlCommand sqlcmd)
+        {
+            sqlcmd.Parameters.AddWithValue("@jobid",textJobid.Text);
+            sqlcmd.Parameters.AddWithValue("@fname",textFname.Text);
+            sqlcmd.Parameters.AddWithValue("@lname",textLname.Text);
+            sqlcmd.Parameters.AddWithValue("@email",textEmail.Text);
+            sqlcmd.Parameters.AddWithValue("@sal",textSal.Text);
+        }
+
+
+
+        public void emptyrecord() //After datas are successfully updated or inserted or deleted, all the text boxes empty automatically
+        {
+            textJobid.Text = "";
+            textFname.Text = "";
+            textLname.Text = "";
+            textEmail.Text = "";
+            textSal.Text = "";
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textJobid_TextChanged(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string strcon= ConfigurationManager.ConnectionStrings["first_config"].ConnectionString;
+            SqlConnection conn1 = new SqlConnection(strcon);
+
+
+            if (textJobid.Text.Length == 0 || textFname.Text.Length == 0 || textLname.Text.Length == 0 || textEmail.Text.Length == 0 || textSal.Text.Length == 0)
+            {
+                mustinput();
+            }
+            else
+            {
+                try
+                {
+                    conn1.Open();
+
+                    SqlCommand checkexistdata = new SqlCommand("select JobId from table1 where JobId='" + textJobid.Text + "'", conn1);
+                    SqlDataAdapter adapter = new SqlDataAdapter(checkexistdata);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        MessageBox.Show("This Job ID already exist.\nEnter Your Job ID");
+                        textJobid.Text = "";
+                    }
+                    else
+                    {
+
+                        string query = "insert into table1 values(@jobid,@fname,@lname,@email,@sal)";
+                        SqlCommand cmd = new SqlCommand(query, conn1);
+                        scommand(cmd);
+
+                        int i = cmd.ExecuteNonQuery();
+
+
+                        conn1.Close();
+
+                        if (i > 0) // if insert operation executed successfully then it return 1 else 0
+                        {
+                            MessageBox.Show("Data Inserted Successfully");
+                            emptyrecord(); //After datas are successfully insertedted, all the text boxes empty automatically
+
+                            /*textJobid.Text = ""; //we can use it directly and also can use it as make a function for more use
+                            textFname.Text = "";
+                            textLname.Text = "";
+                            textEmail.Text = "";
+                            textSal.Text = "";*/
+                        }
+                        else
+                        {
+                            MessageBox.Show("Data not Inserted");
+                        }
+                    }
+                }
+                catch (Exception em)
+                {
+                    MessageBox.Show(em.Message.ToString());
+                }
+            }
+
+        }
+
+
+
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string strcon = ConfigurationManager.ConnectionStrings["first_config"].ConnectionString;
+            SqlConnection conn2=new SqlConnection(strcon);
+
+
+            if (textJobid.Text.Length == 0 || textFname.Text.Length == 0 || textLname.Text.Length == 0 || textEmail.Text.Length == 0 || textSal.Text.Length == 0)
+            {
+                mustinput();
+            }
+            else
+            {
+
+                try
+                {
+                    conn2.Open();
+                    String query = "update table1 set FirstName=@fname,LastName=@lname,Email=@email,Salary=@sal where JobId=@jobid";
+                    SqlCommand cmd = new SqlCommand(query, conn2);
+                    scommand(cmd);
+
+                    int u = cmd.ExecuteNonQuery();
+                    conn2.Close();
+
+                    if (u > 0) // if update operation executed successfully then it return 1 else 0
+                    {
+                        MessageBox.Show("Data Updated Successfully");
+                        emptyrecord(); //After datas are successfully updated, all the text boxes empty automatically
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Not Updated Successfully");
+                    }
+
+                }
+                catch (Exception em)
+                {
+                    MessageBox.Show(em.Message.ToString());
+                }
+            }
+        }
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string strcon = ConfigurationManager.ConnectionStrings["first_config"].ConnectionString;
+            SqlConnection conn3 = new SqlConnection(strcon);
+
+
+
+            if (textJobid.Text == "") {
+                MessageBox.Show("Please Enter Job ID.");
+                textJobid.Focus();
+            }
+            else
+            {
+                try
+                { 
+                    conn3.Open();
+
+                    String query = "delete from table1 where JobId=@jobid";
+                    SqlCommand cmd=new SqlCommand(query, conn3);
+                    cmd.Parameters.AddWithValue("@jobid",textJobid.Text);
+                    int d=cmd.ExecuteNonQuery();
+
+                    conn3.Close();
+
+                    if (d > 0) // if update operation executed successfully then it return 1 else 0
+                    {
+                        MessageBox.Show("Data Deleted Successfully");
+                        emptyrecord(); //After datas are successfully deleted, all the text boxes empty automatically
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Not Deleted Successfully");
+                    }
+
+                }
+                catch (Exception em)
+                {
+                    MessageBox.Show(em.Message.ToString());
+                }
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            String strcon = ConfigurationManager.ConnectionStrings["first_config"].ConnectionString;
+            SqlConnection conn4=new SqlConnection(strcon);
+
+            conn4.Open();
+
+            SqlCommand cmd=new SqlCommand("select*from table1",conn4);
+
+            SqlDataAdapter adapter=new SqlDataAdapter(cmd);
+            DataTable dt=new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource= dt;
+
+            conn4.Close();
+        }
+    }
+}
